5fc6fec5e7cb9472705f368b3d52c3b184809827
okanozman-create <okanozman88@gmail.com>
2024-01-20 20:42:23 +0300

    new  API GATEAWAY

--- src/Phone.js
+++ src/Phone.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 
